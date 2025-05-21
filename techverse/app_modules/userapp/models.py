02687d7987f35224c7ac7d3f2250a86f7a2fc038
Sahilkanjariya <sahilkanjariya4@gmail.com>
2025-05-21 16:26:31 +0530

    sahil-first

--- techverse/app_modules/userapp/models.py
+++ techverse/app_modules/userapp/models.py
@@ -19,3 +19,27 @@ class Industries(BaseModel):
     title = models.CharField(max_length=255, null=True,blank=True)
     description = models.CharField(max_length=255, null=True,blank=True)
     
+class IndustrieCategory(models.Model):
+    name = models.CharField(max_length=255, null=True,blank=True)
+    description = models.CharField(max_length=255, null=True,blank=True)
+    
+class Blog(models.Model):
+    name = models.CharField(max_length=255, null=True, blank=True)
+    description = models.CharField(max_length=255, null=True,blank=True)
+    date = models.DateField(null=True,blank=True)
+    image = models.ImageField(upload_to='blog_img',null=True,blank=True)
+    
+class Career(models.Model):
+    first_name = models.CharField(max_length=255, null=True, blank=True)
+    last_name = models.CharField(max_length=255, null=True, blank=True)
+    experience = models.IntegerField(null=True, blank=True)
+    position = models.CharField(max_length=255, null=True,blank=True)
+    cv = models.ImageField(upload_to='blog_img',null=True,blank=True)
+    message = models.CharField(max_length=255, null=True,blank=True)
+    
+class GetInTouch(models.Model):
+    name = models.CharField(max_length=255, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    phone = models.IntegerField(null=True, blank=True)
+    city = models.CharField(max_length=255, null=True, blank=True)
+    message = models.CharField(max_length=255, null=True, blank=True)
