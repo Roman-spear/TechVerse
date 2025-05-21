d511d751aa92c23a635d7b9d482a116917660df3
Sahilkanjariya <sahilkanjariya4@gmail.com>
2025-05-21 18:34:26 +0530

    Merge branch 'main' of https://github.com/Lucifier1/TechVerse

--- techverse/app_modules/userapp/models.py
+++ techverse/app_modules/userapp/models.py
@@ -2,3 +2,11 @@ from django.db import models
 from app_modules.base.models import BaseModel
 
 # Create your models here.
+
+
+class ContactEnquiry(BaseModel):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone = models.BigIntegerField()
+    
--- techverse/app_modules/userapp/views.py
+++ techverse/app_modules/userapp/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView,ListView,UpdateView,DeleteView,DetailView,View,CreateView
 from django.contrib import messages
-from app_modules.userapp import models
-from app_modules.userapp import forms 
+from app_modules.adminapp import models
+from app_modules.adminapp import forms 
 from django.urls import reverse_lazy
 
 # Create your views here.
@@ -10,6 +10,13 @@ from django.urls import reverse_lazy
 
 class UserIndexView(TemplateView):
     template_name = "userapp/index.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["servicecategory"] = models.ServiceCategory.objects.all()
+        context["industrycategory"] = models.IndustryCategory.objects.all()
+        return context
+    
     
 class UserAboutusView(TemplateView):
     template_name = "userapp/about.html"
