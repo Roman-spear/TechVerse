43424062225d86e0188bfc49def142b7af1f21c5
Lucifier1 <90929820+Lucifier1@users.noreply.github.com>
2025-06-21 17:04:52 +0530

    Changes-21/6-1

--- techverse/techverse/settings.py
+++ techverse/techverse/settings.py
@@ -136,6 +136,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR/'static']
+STATIC_ROOT = '/static_tech/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
